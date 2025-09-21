f37acfb2749130633f316c2065bba35860163274
Trader1991 <ketik.iq1000@gmail.com>
2025-09-21 12:41:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-Tabletop.init({
-  key: 'https://docs.google.com/spreadsheets/d/1YMnA04wSPLXowwML3MkilCD4MEUehontx8DoXIKGdvI/pubhtml',
-  simpleSheet: false,
-  wantedSheets: ['ttotal presentasi'], // atau nama tab yang sesuai
-  callback: function(data, tabletop) {
-    const sheetData = data['ttotal presentasi'].elements;
-    const tbody = document.querySelector('#portfolio-table tbody');
-    tbody.innerHTML = '';
+window.addEventListener('DOMContentLoaded', () => {
+  const sheetURL = 'https://docs.google.com/spreadsheets/d/1YMnA04wSPLXowwML3MkilCD4MEUehontx8DoXIKGdvI/pubhtml';
 
-    sheetData.forEach(row => {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-        <td>${row.modal || '-'}</td>
-        <td>${row.no || '-'}</td>
-        <td>${row.tanggal || '-'}</td>
-        <td>${row.profit || '-'}</td>
-        <td>${row.keuntungan_dari_modal || '-'}</td>
-        <td>${row.total_presentasi || '-'}</td>
-      `;
-      tbody.appendChild(tr);
-    });
-  }
+  Tabletop.init({
+    key: sheetURL,
+    simpleSheet: false,
+    wantedSheets: ['ttotal presentasi'], // pastikan nama tab persis seperti ini
+    callback: function(data, tabletop) {
+      const sheetData = data['ttotal presentasi'].elements;
+      const tbody = document.querySelector('#portfolio-table tbody');
+      tbody.innerHTML = '';
+
+      sheetData.forEach(row => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+          <td>${row.modal || '-'}</td>
+          <td>${row.no || '-'}</td>
+          <td>${row.tanggal || '-'}</td>
+          <td>${row.profit || '-'}</td>
+          <td>${row['keuntungan dari modal'] || '-'}</td>
+          <td>${row['total presentasi'] || '-'}</td>
+        `;
+        tbody.appendChild(tr);
+      });
+    }
+  });
 });
