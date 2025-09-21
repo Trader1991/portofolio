d4770d0b46bd79f48b357594e075f7fe9ce26e60
Trader1991 <ketik.iq1000@gmail.com>
2025-09-21 12:36:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-Tabletop.init({
-  key: 'https://docs.google.com/spreadsheets/d/1YMnA04wSPLXowwML3MkilCD4MEUehontx8DoXIKGdvI/pubhtml',
-  simpleSheet: false,
-  wantedSheets: ['total presentasi'],
-  callback: function(data, tabletop) {
-    const sheetData = data['total presentasi'].elements;
-    const tbody = document.querySelector('#portfolio-table tbody');
-    tbody.innerHTML = '';
+window.addEventListener('DOMContentLoaded', () => {
+  Tabletop.init({
+    key: 'https://docs.google.com/spreadsheets/d/1YMnA04wSPLXowwML3MkilCD4MEUehontx8DoXIKGdvI/pubhtml',
+    simpleSheet: false,
+    wantedSheets: ['ttotal presentasi'], // atau 'total presentasi' sesuai nama tab kamu
+    callback: function(data, tabletop) {
+      const sheetData = data['ttotal presentasi'].elements;
+      const tbody = document.querySelector('#portfolio-table tbody');
+      tbody.innerHTML = '';
 
-    sheetData.forEach(row => {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-        <td>${row.modal || '-'}</td>
-        <td>${row.no || '-'}</td>
-        <td>${row.tanggal || '-'}</td>
-        <td>${row.profit || '-'}</td>
-        <td>${row.keuntungan_dari_modal || '-'}</td>
-        <td>${row.total_presentasi || '-'}</td>
-      `;
-      tbody.appendChild(tr);
-    });
-  }
+      sheetData.forEach(row => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+          <td>${row.modal || '-'}</td>
+          <td>${row.no || '-'}</td>
+          <td>${row.tanggal || '-'}</td>
+          <td>${row.profit || '-'}</td>
+          <td>${row.keuntungan_dari_modal || '-'}</td>
+          <td>${row.total_presentasi || '-'}</td>
+        `;
+        tbody.appendChild(tr);
+      });
+    }
+  });
 });
